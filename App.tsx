880ef56b406b074d5da6aeedcdcfee0367b8fb57
Shawaiz Ali Rehman <shawaiali788@gmail.com>
2026-06-30 20:07:00 +0800

    Added:React hook form-with validation

--- App.tsx
+++ App.tsx
@@ -11,29 +11,49 @@ import {
 } from 'react-native';
 import CustomInput from './src/components/CustomInput';
 import CustomButton from './src/components/CustomButton';
+import { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form'
 
 export default function App() {
+
+
+
+
+  const { control, handleSubmit, formState: { errors } } = useForm({})
+
+  const OnSignInPress = (data: any) => {
+    console.log('SignIn:', data);
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}
     >
-      <StatusBar style="auto" />
       <Text style={styles.title}>Sign in</Text>
 
-      <CustomInput
-        placeholder='Email'
-        autoFocus
-        autoCapitalize='none'
-        keyboardType='email-address'
-        autoComplete='email'
-      />
-      <CustomInput placeholder='Password' secureTextEntry />
+
+      <View style={styles.form}>
+
+
+        <CustomInput
+          placeholder='Email'
+          name='email'
+          autoFocus
+          autoCapitalize='none'
+          keyboardType='email-address'
+          autoComplete='email'
+          control={control}
+        />
+        <CustomInput
+          placeholder='Password'
+          secureTextEntry
+          control={control}
+          name='password'
+        />
+      </View>
       <CustomButton
         text='Sign in'
-        onPress={() => {
-          console.log('Pressed');
-        }}
+        onPress={handleSubmit(OnSignInPress)}
       />
 
       <StatusBar style='auto' />
@@ -52,6 +72,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '600',
-  },
+  }, form: {
+    gap: 5
+  }
 
 });
--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,11 +1,30 @@
-import { TextInput, StyleSheet, TextInputProps } from 'react-native';
-
+import { TextInput, StyleSheet, TextInputProps, Text, View } from 'react-native';
+import { Controller } from 'react-hook-form'
 type CustomInputProps = {
-    // custom fields
+    control: any;
+    name: string
 } & TextInputProps;  //merges with text input props
 
-export default function CustomInput(props: CustomInputProps) {
-    return <TextInput {...props} style={[styles.input, props.style]} />; //so that we can allow overwriting of props as well
+export default function CustomInput({ control, name, ...props }: CustomInputProps) {
+    return (<Controller
+        name={name}
+        control={control}
+        rules={{ required: 'This field is required' }}
+        render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
+            <View style={styles.container}>
+                <TextInput
+                    {...props}
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    style={[styles.input, props.style]}
+
+                />
+                <Text style={styles.error}>{error?.message}</Text>
+            </View>
+        )} />
+
+    ); //so that we can allow overwriting of props as well
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +34,9 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         borderColor: '#ccc',
         width: '80%',
-    },
+    }, error: {
+        color: 'crimson'
+    }, container: {
+        gap: 2
+    }
 });
